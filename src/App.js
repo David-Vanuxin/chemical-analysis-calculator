5f689585e9f26950b87b40aa72d6ed60aa8565ba
David-Vanuxin <2468steeld@gmail.com>
2022-06-03 22:01:43 +0300

    Слияния eedux и react-router-dom

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Counter from "./components/Counter"
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/counter">Counter</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/about" caseSensitive={false} element={<About />} />
+          <Route path="/users" caseSensitive={false} element={<Users />} />
+          <Route path="/" caseSensitive={false} element={<Home />} />
+          <Route path="/counter" caseSensitive={false} element={<Counter />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
