8da0b9931888eb1d1766fc20a294b09b4efd3484
David-Vanuxin <2468steeld@gmail.com>
2022-06-06 13:25:16 +0300

    Provaider has been used

--- src/components/InputSubstance.js
+++ src/components/InputSubstance.js
@@ -1,16 +1,21 @@
-import changeElementsListReducer from "../redux/changeElementsListReducer"
+//import changeElementsListReducer from "../redux/changeElementsListReducer"
 import React, {useState} from "react"
-import {createStore} from "redux"
+import {useDispatch, useSelector} from 'react-redux'
+//import {createStore} from "redux"
 
-let store = createStore(changeElementsListReducer)
+/*let store = createStore(changeElementsListReducer)
 // Эти продукты всегда должны быть
 store.dispatch({ type:"addElement", name: "CO2"})
-store.dispatch({ type:"addElement", name: "H2O"})
+store.dispatch({ type:"addElement", name: "H2O"})*/
 
 export default function InputSubstance(props) {
 	const [state, setState] = useState(false)
 
-	return (<label>
+  const dispatch = useDispatch()
+  const data = useSelector(state => state.data)
+
+	return (
+		<label>
 			<input type="checkbox" onClick={() => changeElement(props.name)}></input>
 			<span id={props.name}>{props.name}</span>
 		</label>
